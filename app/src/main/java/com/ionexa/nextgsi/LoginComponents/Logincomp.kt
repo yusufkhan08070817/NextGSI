5869c120a36f7c29bd31363fb28fe67ba2460293
yusufkhan08070817 <khanyusuk340@gmail.com>
2024-07-10 21:04:05 +0530

    LoginPage

--- app/src/main/java/com/ionexa/nextgsi/LoginComponents/Logincomp.kt
+++ app/src/main/java/com/ionexa/nextgsi/LoginComponents/Logincomp.kt
@@ -37,10 +37,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
