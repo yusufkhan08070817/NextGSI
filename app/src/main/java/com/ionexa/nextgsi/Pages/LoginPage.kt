31615e359f0852696c5bb86f2abb0f5b4f69039f
yusufkhan08070817 <khanyusuk340@gmail.com>
2024-08-02 01:50:30 +0530

    email Verificatation completed

--- app/src/main/java/com/ionexa/nextgsi/Pages/LoginPage.kt
+++ app/src/main/java/com/ionexa/nextgsi/Pages/LoginPage.kt
@@ -1,6 +1,7 @@
 package com.ionexa.nextgsi.Pages
 
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
@@ -19,6 +20,10 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -33,6 +38,7 @@ import com.ionexa.nextgsi.Components.ButtonWithCutCornerShape
 import com.ionexa.nextgsi.Components.Combined
 import com.ionexa.nextgsi.Components.Dropdownroll
 import com.ionexa.nextgsi.Components.Email
+import com.ionexa.nextgsi.Components.LoadingScreen
 import com.ionexa.nextgsi.Components.LogWithGoogleFacebookApple
 import com.ionexa.nextgsi.Components.Login
 import com.ionexa.nextgsi.Components.Logn_Register
@@ -40,17 +46,25 @@ import com.ionexa.nextgsi.Components.Password
 import com.ionexa.nextgsi.Components.Register
 import com.ionexa.nextgsi.Components.RememberAndForgot
 import com.ionexa.nextgsi.DataClass.SignInState
+import com.ionexa.nextgsi.FIreBase.FirebaseAuthManager
 import com.ionexa.nextgsi.MVVM.Loginmvvm
 import com.ionexa.nextgsi.R
 import com.ionexa.nextgsi.SingleTon.NaveLabels
 import com.ionexa.nextgsi.ui.theme.DarkOrchidwebcolor
 import com.ionexa.nextgsi.ui.theme.Standardpurple
+import kotlinx.coroutines.delay
 
 @Composable
-fun LoginPage( LoginViewModel: Loginmvvm,navController: NavController,state: SignInState,onGoogleSignInClick: () -> Unit) {
-
+fun LoginPage(
+    LoginViewModel: Loginmvvm,
+    FBauthManager: FirebaseAuthManager,
+    navController: NavController,
+    state: SignInState,
+    onGoogleSignInClick: () -> Unit
+) {
+    var loadingScreen by remember { mutableStateOf(false) }
     val context = LocalContext.current
-    LaunchedEffect(key1 = state.signInError)  {
+    LaunchedEffect(key1 = state.signInError) {
         state.signInError?.let { error ->
             Toast.makeText(context, error, Toast.LENGTH_LONG).show()
         }
@@ -60,6 +74,29 @@ fun LoginPage( LoginViewModel: Loginmvvm,navController: NavController,state: Sig
     LaunchedEffect(key1 = state.isSignSuccessfull) {
         if (state.isSignSuccessfull) {
             navController.navigate(NaveLabels.Home)
+        }
+    }
+    LaunchedEffect(loadingScreen) {
+        // Loop to periodically check email verification status
+        while (loadingScreen) {
+            delay(1000) // Delay for 1 second
+
+            // Reload the current user
+            FBauthManager.getCurrentUser()?.reload()?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val isEmailVerified = FBauthManager.getCurrentUser()?.isEmailVerified == true
+                    if (isEmailVerified) {
+                        // Navigate to the next screen if email is verified
+                        navController.navigate(NaveLabels.OTPVerificatation)
+                        Log.e("TAG", "Email verified")
+                        return@addOnCompleteListener
+                    } else {
+                        Log.e("TAG", "Email not verified")
+                    }
+                } else {
+                    Log.e("TAG", "Failed to reload user")
+                }
+            }
         }
     }
 
@@ -93,54 +130,62 @@ fun LoginPage( LoginViewModel: Loginmvvm,navController: NavController,state: Sig
         ) {
             Login()
             Combined(Modifier)
-            Logn_Register( Modifier,LoginViewModel.LoginAndRigister) {
+            Logn_Register(Modifier, LoginViewModel.LoginAndRigister) {
                 LoginViewModel.onLoginAndRigister()
             }
             AnimatedVisibility(visible = LoginViewModel.LoginAndRigister) {
-               Column {
-                   Email(Email = LoginViewModel.email,error=LoginViewModel.emailerror) {
-                       LoginViewModel.setemail(it)
-                   }
-                   Spacer(modifier = Modifier.height(30.dp))
-                   Password(Password = LoginViewModel.password,error=LoginViewModel.passworderror) {
-                       LoginViewModel.setpassword(it)
-                   }
-                   RememberAndForgot(IsChecked = LoginViewModel.remember) {
-                       LoginViewModel.onremember()
-                   }
-               }
+                Column {
+                    Email(Email = LoginViewModel.email, error = LoginViewModel.emailerror) {
+                        LoginViewModel.setemail(it)
+                    }
+                    Spacer(modifier = Modifier.height(30.dp))
+                    Password(
+                        Password = LoginViewModel.password,
+                        error = LoginViewModel.passworderror
+                    ) {
+                        LoginViewModel.setpassword(it)
+                    }
+                    RememberAndForgot(IsChecked = LoginViewModel.remember) {
+                        LoginViewModel.onremember()
+                    }
+                }
             }
             AnimatedVisibility(visible = !LoginViewModel.LoginAndRigister) {
-               Column {
-                   Register(Name = LoginViewModel.name,
-                       PhoneNumber = LoginViewModel.phone,
-                       Address = LoginViewModel.address,
+                Column {
+                    Register(Name = LoginViewModel.name,
+                        PhoneNumber = LoginViewModel.phone,
+                        Address = LoginViewModel.address,
 
-                       setName = { name ->
-                           LoginViewModel.setname(name)
-                       },
-                       setPhoneNumber = { phone ->
-                           LoginViewModel.setphone(phone)
-                       },
-                       setAddress = { address ->
-                           LoginViewModel.setaddress(address)
-                       })
-                   Spacer(modifier = Modifier.height(10.dp))
-                   Email(Email = LoginViewModel.email,error=LoginViewModel.emailerror) {
-                       LoginViewModel.setemail(it)
-                   }
-                   Spacer(modifier = Modifier.height(10.dp))
-                   Password(Password = LoginViewModel.password, error = LoginViewModel.passworderror) {
-                       LoginViewModel.setpassword(it)
-                   }
-                   Spacer(modifier = Modifier.height(10.dp))
-                   Row(modifier = Modifier
-                       .padding()
-                       .fillMaxWidth(1f), horizontalArrangement = Arrangement.Center) {
-                       Dropdownroll()
-                   }
-                   Spacer(modifier = Modifier.height(10.dp))
-               }
+                        setName = { name ->
+                            LoginViewModel.setname(name)
+                        },
+                        setPhoneNumber = { phone ->
+                            LoginViewModel.setphone(phone)
+                        },
+                        setAddress = { address ->
+                            LoginViewModel.setaddress(address)
+                        })
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Email(Email = LoginViewModel.email, error = LoginViewModel.emailerror) {
+                        LoginViewModel.setemail(it)
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Password(
+                        Password = LoginViewModel.password,
+                        error = LoginViewModel.passworderror
+                    ) {
+                        LoginViewModel.setpassword(it)
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Row(
+                        modifier = Modifier
+                            .padding()
+                            .fillMaxWidth(1f), horizontalArrangement = Arrangement.Center
+                    ) {
+                        Dropdownroll()
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                }
             }
             if (LoginViewModel.LoginAndRigister) {
 
@@ -148,46 +193,81 @@ fun LoginPage( LoginViewModel: Loginmvvm,navController: NavController,state: Sig
 
             }
 
-
+            AnimatedVisibility(visible = loadingScreen) {
+                LoadingScreen()
+            }
             Spacer(modifier = Modifier.height(10.dp))
             ButtonWithCutCornerShape(LoginViewModel.email,
                 LoginViewModel.password,
                 LoginAndRigister = LoginViewModel.LoginAndRigister,
                 authSignIn = { email, password ->
-                    if (email.isEmpty())
-                    {
+                    if (email.isEmpty()) {
                         LoginViewModel.onemailerror(false)
                         return@ButtonWithCutCornerShape
-                    }else{
+                    } else {
                         LoginViewModel.onemailerror(true)
                     }
-                    if (password.isEmpty())
-                    {
+                    if (password.isEmpty()) {
                         LoginViewModel.onpassworderror(false)
                         return@ButtonWithCutCornerShape
-                    }else{
+                    } else {
                         LoginViewModel.onpassworderror(true)
                     }
-                    //AuthViewMOdel.signInWithEmail(email, password)
-                    navController.navigate(NaveLabels.Home)
+                  FBauthManager.signInWithEmail(
+                      email=email,
+                      password=password, onComplete = {
+                          status, user, error ->
+                          if (status) {
+                              navController.navigate(NaveLabels.Home)
+                          } else {
+                              Toast.makeText(context, error, Toast.LENGTH_LONG).show()
+                          }
+                      }
+                  )
+
 
                 },
                 authSignUp = { email, password ->
-                    if (email.isEmpty())
-                    {
+                    if (email.isEmpty()) {
                         LoginViewModel.onemailerror(true)
                         return@ButtonWithCutCornerShape
-                    }else{
+                    } else {
                         LoginViewModel.onemailerror(false)
                     }
-                    if (password.isEmpty())
-                    {
+                    if (password.isEmpty()) {
                         LoginViewModel.onpassworderror(true)
                         return@ButtonWithCutCornerShape
-                    }else{
+                    } else {
                         LoginViewModel.onpassworderror(false)
                     }
-                  //  AuthViewMOdel.signUpWithEmail(email, password)
+                    FBauthManager.createAccountWithEmail(
+                        email = email,
+                        password = password,
+                        stateloader = {
+loadingScreen=it
+                        }
+                    )
+                    { status, user, error ->
+                        if (status) {
+                            FBauthManager.checkEmailVerification(
+                                user=FBauthManager.getCurrentUser(),
+                                stateloader = {
+                                    loadingScreen = it
+                                }, onComplete = {
+                                    status, user, error ->
+                                    if (status) {
+                                        navController.navigate(NaveLabels.Home)
+                                    } else {
+                                        Toast.makeText(context, error, Toast.LENGTH_LONG).show()}
+
+                                }
+                            )
+                            navController.navigate(NaveLabels.OTPVerificatation)
+                        } else {
+                            Toast.makeText(context, error, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                    //  AuthViewMOdel.signUpWithEmail(email, password)
 
                 })
             LogWithGoogleFacebookApple(GoogleAuth = {
